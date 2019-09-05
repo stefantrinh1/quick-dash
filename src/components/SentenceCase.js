4a43f52f76bc09c6ead3b3bb081bb7cc2369f2fe
unset <unset>
2019-09-04 21:49:05 -0600

    reducing code on sentance case

--- src/components/SentenceCase.js
+++ src/components/SentenceCase.js
@@ -3,11 +3,8 @@ import ButtonIcon from "../media/icons/capitialize.svg"
 
 const SentenceCase = () => {
 
-     
-    function firstLetterUpper(theString) {
-        var newString = theString.toLowerCase().replace(/(^\s*\w|[\.\!\?]\s*\w)/g,function(c){return c.toUpperCase()});
-      return newString;
-    }
+    //  this uses regex to find the full  stop and replace the first letter with a upper case letter after it.
+    const firstLetterUpper = (theString) => theString.toLowerCase().replace(/(^\s*\w|[\.\!\?]\s*\w)/g,function(c){return c.toUpperCase()});
 
     const handleClick = () => {
     // grab the Text area Element to work with.
@@ -19,7 +16,6 @@ const SentenceCase = () => {
         textArea.value = str
     } 
     
-
     return (<button onClick={handleClick}><img style={{height:'0.75em'}} className="TextOperations__ButtonIcon" src={ButtonIcon} /> | Sentence Case</button>)
     
 }
